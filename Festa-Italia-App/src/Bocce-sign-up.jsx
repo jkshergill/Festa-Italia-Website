7fc403bae8a289bf7f592db9fe8c1757cfedcd59
Ryan Lee <ryanplee@csus.edu>
2025-10-30 23:07:04 -0700

    Added the team sponsor textbox per clients request

--- Festa-Italia-App/src/Bocce-sign-up.jsx
+++ Festa-Italia-App/src/Bocce-sign-up.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './bocce-sign-up.css'; //src/bocce-sign-up.css
+//src/bocce-sign-up.css
+import './bocce-sign-up.css'; 
 
 export default function BocceSignUp() {
   // Prevent full page reload on submit; ***PLUG IN submit logic later***
@@ -9,7 +9,7 @@ export default function BocceSignUp() {
 
   return (
     // Using a wrapper instead of <body>. "body" class for styling.
-    <div className="body">
+    <div className="Bocce-signup-body">
       {/* Logo (place logo2.gif in /public so it serves at /logo2.gif) */}
       <img
         src="/logo2.gif"              // If you put logo2.gif in the project's /public folder
@@ -25,10 +25,15 @@ export default function BocceSignUp() {
         {/* Form */}
         <form className="signup-form" onSubmit={handleSubmit}>
           {/* Team name */}
-          <label htmlFor="team-name" className="team-label">
-            What is the name of your team?
-          </label>
-          <input type="text" id="team-name" name="team-name" required />
+          <div className="players-list team-input">
+            <label>What is the name of your team:</label>
+            <input
+              type="text"
+              id="teamName"
+              name="teamName"
+              placeholder="Team Name"
+            />
+          </div>
 
           {/* Players heading */}
           <p className="players-heading">Who will be your team members?</p>
@@ -36,16 +41,24 @@ export default function BocceSignUp() {
             {/* Player inputs */}
             <div className="players-list">
               <label htmlFor="player1-name">1.</label>
-              <input type="text" id="player1-name" name="player1-name" required />
+              <input type="text" id="player1-name" name="player1-name" placeholder="Player 1 name" required />
 
               <label htmlFor="player2-name">2.</label>
-              <input type="text" id="player2-name" name="player2-name" required />
+              <input type="text" id="player2-name" name="player2-name" placeholder="Player 2 name" required />
 
               <label htmlFor="player3-name">3.</label>
-              <input type="text" id="player3-name" name="player3-name" required />
+              <input type="text" id="player3-name" name="player3-name" placeholder="Player 3 name" required />
 
               <label htmlFor="player4-name">4.</label>
-              <input type="text" id="player4-name" name="player4-name" required />
+              <input type="text" id="player4-name" name="player4-name" placeholder="Player 4 name" required />
+
+              <label>Sponsor:</label>
+              <input
+                type="text"
+                id="sponsorName"
+                name="sponsorName"
+                placeholder="Sponsor's name"
+              />
             </div>
 
             {/* Bocce balls image */}
