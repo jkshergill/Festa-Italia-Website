c3efe439b81e92724253277ff78863e49dc1dfc5
jkshergill <jagjeet2504@gmail.com>
2025-10-31 18:31:14 -0700

    #4, #29, #31, #35, #101, #102, #104; Converted login page content into react-friendly .jsx file, created page to purchase coronation ball tickets as a .jsx file as well as a .html file as a backup to view the page if needed

---
+++ Festa-Italia-App/src/Login.jsx
@@ -0,0 +1,29 @@
+import './Login.css';
+
+function Login() {
+    return (
+        <div className="form__container">
+            <form autoComplete="off" className="form" method="POST">
+                <p className="form__title">Login</p>
+                <p className="form__message">Login now to view your dashboard</p>
+                    <div className="form__group">
+                        <label>
+                            <input type="text" name="FirstName" placeholder="First Name" required />
+                        </label>
+                        <label>
+                            <input type="text" name="LastName" placeholder="Last Name" required />
+                        </label>
+                    </div>
+                    <label>
+                        <input type="text" name="Email" placeholder="Email" required />
+                    </label>
+                    <label>
+                        <input type="password" name="Password" placeholder="Password" required />
+                    </label>
+                    <button type="submit" className="form__submit">Submit</button>
+                    <p className="form__login--redirect">Don't have an account? <a href="#" className="form__signup--link">Sign Up</a></p>
+                    <p className="form__forgot-password"><a href="#" className="form__forgot-password--link">Forgot Password?</a></p>
+            </form>
+        </div>
+    );
+}
