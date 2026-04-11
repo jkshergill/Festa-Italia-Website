79b131b4c0633961bcee5bb2de502534abb9fa75
Pratish2203 <pratishpatil@csus.edu>
2026-04-11 12:34:13 -0700

    Updated Donation.jsx

--- Festa-Italia-App/src/Donation.jsx
+++ Festa-Italia-App/src/Donation.jsx
@@ -3,7 +3,7 @@ import './Donation.css';
 import './HomePage.css';
 import { supabase } from './supabaseClient';
 
-export default function Donation() {
+export default function Donation({setPage}) {
   const [start, setStart] = useState({ month: '', day: '', year: '' });
   const [end, setEnd] = useState({ month: '', day: '', year: '' });
 
@@ -503,11 +503,12 @@ export default function Donation() {
         </section>
       </main>
 
-      <a href="/donate" className="floating-donate" aria-label="Donate">
+      <a  className="floating-donate" aria-label="Donate">
         <svg className="heart" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
           <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
         </svg>
-        <span className="donate-badge">Donate</span>
+        <span className="donate-badge" onClick={()=> setPage("donate")}>Donate</span>
+        {/* <span className="donate-badge">Donate</span> */}
       </a>
     </div>
   );
